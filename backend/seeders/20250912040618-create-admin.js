7200066deeb248d2fc8c6994e6004306940bbe52
imam <kzuto24@gmail.com>
2025-09-12 11:07:03 +0700

    feat: add admin user seeder with hashed password and role assignment

---
+++ backend/seeders/20250912040618-create-admin.js
@@ -0,0 +1,22 @@
+"use strict";
+const bcrypt = require("bcryptjs");
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const hashedPassword = await bcrypt.hash("admin123", 10);
+    return queryInterface.bulkInsert("users", [
+      {
+        username: "User Admin",
+        email: "admin@mail.com",
+        password: hashedPassword,
+        role: "admin",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("users", { email: "admin@mail.com" }, {});
+  },
+};
